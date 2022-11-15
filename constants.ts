5a2ca6205fd23d44c42796f89267bd4be98c6525
kwisnia <230037@edu.p.lodz.pl>
2022-11-15 16:23:55 +0100

    Add prod api url

--- constants.ts
+++ constants.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.gamepark.space"
+    : "http://localhost:8080";
 
 export const axiosClient = axios.create({
   baseURL: "http://localhost:8080",
